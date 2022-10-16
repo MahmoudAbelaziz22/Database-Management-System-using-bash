9c80bb9118bcb104ec1e6cbe903e2971062aae0a
MahmoudAbelaziz22 <mahmoud.abdelaziz.mahmoud22@gmail.com>
2022-10-16 11:55:12 +0200

    creating database main file.

---
+++ database-management-system.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+#
+# Databse management system project main file.
+
+# including helper function file.
+source ./helper-functions.sh
+
+welcome_message
+
+while true
+do 
+    if [ ! -d "./Databases" ]; then
+    mkdir  ./Databases
+    fi
+    print_colored "green" "+------------------------------+"
+    print_colored "green" "|------------DBMS--------------|"
+    print_colored "green" "|------------------------------|"
+    print_colored "green" "| 1. Dive into Our Database.   |"
+    print_colored "green" "| 2. Exit.                     |"
+    print_colored "green" "+------------------------------+"
+    print_colored "bwhite" "Please Select a Service: \c"
+    read REPLY
+    case $REPLY in
+    1 ) echo "Does not completed."
+    ;;
+    2 ) close_program 
+    ;;
+    * ) print_colored "red" "Invalid choice, Please Select a Correct Service Number."
+    ;;
+    esac
+
+done
---
+++ helper-functions.sh
@@ -0,0 +1,52 @@
+#######################################
+# Print Colored Text to screen.
+# Arguments:
+#   COLOR,Text    
+# Usage:      print_colored "color" "text" 
+#######################################
+function print_colored() {
+   case $1 in
+     "green") COLOR="\033[0;32m" ;;
+     "red") COLOR="\033[0;31m" ;;
+     "white") COLOR="\033[0;37m" ;;
+     "bwhite") COLOR="\033[1;37m" ;;
+     "blue") COLOR="\033[44m" ;;
+      "*") COLOR="\033[0m"
+    esac
+   echo -e "${COLOR} $2 ${NC}" 
+}
+#######################################
+# Print welcome messages to screen.
+# Arguments:
+#   Non
+# Usage:      welcome_message
+#######################################
+function welcome_message() {
+    print_colored "blue" "************************************************************"
+    print_colored "blue" "**             Bash Shell Scripting Project               **"
+    print_colored "blue" "**              Database Management System                **"
+    print_colored "blue" "**                                                        **"
+    print_colored "blue" "**          Created by: Eng. Mahmoud Abdelaziz            **"
+    print_colored "blue" "************************************************************"
+
+}
+#######################################
+# Close the program function.
+# Arguments:
+#   Non
+# Usage:      close_program
+#######################################
+function close_program() {
+    print_colored "green" "+------------------------------+"
+    print_colored "green" "| 1. Yes                       |"
+    print_colored "green" "| 2. No                        |"
+    print_colored "green" "+------------------------------+"
+    print_colored "white" "Are you sure you want to close the program? \c"
+    read REPLY
+        case $REPLY in
+        1 ) exit ;;
+        2 )  ;;
+        * ) print_colored "red" "Invalid choice, Please Select a Correct Service Number." ;;
+        esac
+}
+
